4f5faeebdec3aee963f07c827032625fd173ac27
AkimPopelishko <akimzaya@mail.ru>
2017-05-11 21:36:58 +0300

    Home Commit (faulty)

--- src/com/bot/Line.java
+++ src/com/bot/Line.java
@@ -21,40 +21,99 @@ public class Line extends JPanel {
             arr[0]=0;
             arr[1]=b;
             f=true;
-            System.out.println("b<height && b>0");
+            System.out.println("левый край (1): "+arr[0]+";"+arr[1]);
         }
-        if (-b/k<width && -b/k>0)   //точка на нижнем крае
+        if (-b/k<width && -b/k>0)   //точка на верхнем крае
             if (f) {
                 arr[2] = -b / k;
                 arr[3] = 0;
-                System.out.println("-b/k<width && -b/k>0");
+                System.out.println("верхний край (2): "+arr[2]+";"+arr[3]);
                 return arr;
             }
             else {
                 arr[0] = -b / k;
                 arr[1] = 0;
                 f = true;
-                System.out.println("else");
+                System.out.println("верхний край (1): "+arr[0]+";"+arr[1]);
             }
         if ((height-b)/k<width && (height-b)/k>0)  //точка на верхнем крае
             if (f) {
                 arr[2] = (height-b)/k;
-                arr[3] = width;
-                System.out.println("(height-b)/k<width && (height-b)/k>0");
+                arr[3] = height;
+                System.out.println("нижний край (2): "+arr[2]+";"+arr[3]);
                 return arr;
             }
             else {
                 arr[0] = (height-b)/k;
-                arr[1] = width;
+                arr[1] = height;
                 f = true;
-                System.out.println("else2");
+                System.out.println("нижний край (1): "+arr[0]+";"+arr[1]);
             }
-        if (k * width + b < height && k * width + b > 0 && f) { //точка на правом крае
-            arr[2] = k * width + b;
-            arr[3] = height;
-            System.out.println("k * width + b < height && k * width + b > 0 && f");
-            return arr;
-        }
+        if (k * width + b < height && k * width + b > 0)        //точка на правом крае
+            if (f) {
+                arr[2] = width;
+                arr[3] = k * width + b;
+                System.out.println("правый край (2): "+arr[2]+";"+arr[3]);
+                return arr;
+            }
+            else{
+                arr[0] = width;
+                arr[1] = k * width + b;
+                System.out.println("правый край (1): "+arr[0]+";"+arr[1]);
+                f=true;
+            }
+        if (b==0)
+            if (f) {
+                arr[2]=0;
+                arr[3]=0;
+                System.out.println("левый верхний угол (2): "+arr[2]+";"+arr[3]);
+                return arr;
+            }
+            else{
+                arr[0]=0;
+                arr[1]=0;
+                System.out.println("левый верхний угол (1): "+arr[0]+";"+arr[1]);
+                f = true;
+            }
+        if (b==height)
+            if (f) {
+                arr[2]=0;
+                arr[3]=height;
+                System.out.println("левый нижний угол (2): "+arr[2]+";"+arr[3]);
+                return arr;
+            }
+            else{
+                arr[0]=0;
+                arr[1]=height;
+                System.out.println("левый нижний угол (1): "+arr[0]+";"+arr[1]);
+                f = true;
+            }
+        if (k*width+b==0)
+            if (f) {
+                arr[2]=width;
+                arr[3]=0;
+                System.out.println("правый верхний угол (2): "+arr[2]+";"+arr[3]);
+                return arr;
+            }
+            else{
+                arr[0]=width;
+                arr[1]=0;
+                System.out.println("правый верхний угол (1): "+arr[0]+";"+arr[1]);
+                f = true;
+            }
+        if (k*width+b==height)
+            if (f) {
+                arr[2]=width;
+                arr[3]=height;
+                System.out.println("правый нижний угол (2): "+arr[2]+";"+arr[3]);
+                return arr;
+            }
+            else{
+                arr[0]=width;
+                arr[1]=height;
+                System.out.println("правый нижний угол (1): "+arr[0]+";"+arr[1]);
+                f = true;
+            }
         err.println("Ошибка в методе getEndpoints: не могу найти точки пересечения");   //если почему-то найдено меньше двух точек
         return null;
     }
